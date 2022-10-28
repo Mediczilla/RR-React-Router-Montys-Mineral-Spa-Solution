d734538577b9305d6aca5d97089b22f8239f248f
Joshua <joshua.lopez8893@yahoo.com>
2022-10-27 19:35:06 -0700

    added bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+import NavBar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container';
 
 import './App.css';
 
 import Home from './components/Home'
 import About from './components/About'
 import Packages from './components/Packages'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
 
 
 function App() {
-  
+
   const packages = ['Activate your Crystals', 'Monkey Meditation', 'Soak in the Hotsprings', 'Hypnotherapy', 'Mineral Bath']
 
 
   return (
     <div className="App">
       <Router>
-        <header>
-          <h1 className="title">Welcome to Monty's Mineral SPA</h1>
-
-          <div className="navBar">
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About Us</Link>
-              </li>
-              <li>
-                <Link to="/packages">Our Packages</Link>
-              </li>
-            </ul>
-          </div>
-        </header>
+        <NavBar Bg="Dark" varient='Dark'>
+          <Container>
+            <NavBar.Brand>Ricky's Mineral SPA</NavBar.Brand>
+            <NavBar.Text>
+              <Link to="/">Home</Link>
+            </NavBar.Text>
+            <NavBar.Text>
+              <Link to="/About">About</Link>
+            </NavBar.Text>
+            <NavBar.Text>
+              <Link to="/packages">Our Packages</Link>
+            </NavBar.Text>
+          </Container>
+        </NavBar>
 
         <div className="display">
           <Route path="/" component={Home} />
           <Route path="/about" component={About} />
-          <Route path="/packages" render={() => <Packages packages={packages}/>}  />
+          <Route path="/packages" render={() => <Packages packages={packages} />} />
         </div>
 
       </Router>
--- src/components/Packages.js
+++ src/components/Packages.js
@@ -1,18 +1,30 @@
-
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
 
 export default function Packages(props) {
     const displayPackages = props.packages.map((eachPackage) => <li>{eachPackage}</li>)
 
     return (
-        <div>
-            <div className="packages">
-                <div className="packagesHeader">
-                    <h1>Our Packages</h1>
-                </div>
-                <ul>
-                    {displayPackages}
-                </ul>
-            </div>
-        </div>
+    // <div>
+    //    <div className="packages">
+    //        <div className="packagesHeader">
+    //            <h1>Our Packages</h1>
+    //        </div>
+    //            <ul>
+    //                {displayPackages}
+    //            </ul>
+    //        </div>
+    //    </div>
+<Container>
+    <Card>
+        <Card.Title>Our Packages</Card.Title>
+        <Card.Text></Card.Text>
+        <ListGroup>
+            {displayPackages}
+        </ListGroup>
+    </Card>
+</Container>
+
     )
 }
